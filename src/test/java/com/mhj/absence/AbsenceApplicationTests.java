4936c0066a5bfd67df531561e39e62f25c3178f8
Mohamed <mohamed.jabrane@adservio.fr>
2020-10-20 17:32:27 +0200

    Initial commit

---
+++ src/test/java/com/mhj/absence/AbsenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhj.absence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
